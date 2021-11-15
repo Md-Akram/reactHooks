0c7cf790c831a2c616e67f12f48b48dab955e224
Md-Akram <83151706+Md-Akram@users.noreply.github.com>
2021-11-15 07:50:09 +0600

    Add files via upload

--- hooks.js
+++ hooks.js
@@ -126,3 +126,64 @@ const UseStateCounter = () => {
 
 export default UseStateCounter
 
+//useEffect Basics
+
+import React, { useState, useEffect } from 'react'
+// by default runs after every re-render
+// cleanup function
+// second parameter
+const UseEffectBasics = () => {
+  const [value, setValue] = useState(0)
+
+  useEffect(() => {
+    if (value > 0) {
+      document.title = `new message(${value})`
+    } else {
+      document.title = `React App`
+    }
+  }, [value])
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        Increase
+      </button>
+      <button className='btn' onClick={() => setValue(value - 1)}>
+        Decrease
+      </button>
+    </>
+  )
+}
+
+export default UseEffectBasics
+
+//useEffect Cleanup function
+
+import React, { useState, useEffect } from 'react'
+
+// cleanup function
+// second argument
+
+const UseEffectCleanup = () => {
+  const [size, setSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    return () => {
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+
+  return (
+    <>
+      <h1>{size}px</h1>
+    </>
+  )
+}
+
+export default UseEffectCleanup
