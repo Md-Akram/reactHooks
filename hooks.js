2fbb9d406e2d62392e60e883509da12d5e22ee71
Md-Akram <83151706+Md-Akram@users.noreply.github.com>
2021-11-17 20:04:31 +0600

    Add files via upload

--- hooks.js
+++ hooks.js
@@ -291,3 +291,58 @@ const ShortCircuit = () => {
 }
 
 export default ShortCircuit
+
+
+// form
+
+import React, { useState } from 'react'
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+
+const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [person, setPerson] = useState([])
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
+
+  return (
+    <>
+      <article>
+        <div>
+          <form className='form'>
+            <div className='form-control'>
+              <label htmlFor='firstName'>Name : </label>
+              <input
+                type='text'
+                id='firstName'
+                name='firstName'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+
+            <div className='form-control'>
+              <label htmlFor='email'>Email : </label>
+              <input
+                type='text'
+                id='email'
+                name='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <button onClick={handleClick}>Add Person</button>
+          </form>
+        </div>
+      </article>
+    </>
+  )
+}
+
+export default ControlledInputs
